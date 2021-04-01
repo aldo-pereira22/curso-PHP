682832dfa0569ed74325d743e4c261bfe19c6868
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-04-01 19:59:30 -0300

    css home
    css da home finalizado.

--- PHP_E_WEB/17_agenda/index.php
+++ PHP_E_WEB/17_agenda/index.php
@@ -2,6 +2,8 @@
     include_once("templates/header.php");
 ?>
     <div class="container">
+    <p id="msg">TESTANDO MENSAGEM </p>
+
         <?php if(isset($printMsg) && $printMsg !=''): ?>
             <p id="msg"><?= $printMsg ?> </p>
         <?php endif; ?>
@@ -24,13 +26,13 @@
                 <tbody>
                     <?php foreach($contacts as $contacts): ?>
                         <tr>
-                            <td scope="row"> <?= $contacts['id'] ?>  </td>
+                            <td scope="row" class="col-id"> <?= $contacts['id'] ?>  </td>
                             <td scope="row"> <?= $contacts['name'] ?>  </td>
                             <td scope="row"> <?= $contacts['phone'] ?>  </td>
                             <td class="actions">
                                 <a href="#"> <i class="fas fa-eye check-icon"> </i> </a>
                                 <a href="#"> <i class="far fa-edit edit-icon"> </i> </a>
-                                <button type="submit"> <i class="fas fa-times delete-icon"> </i></button>
+                                <button type="submit" class="delete-btn"> <i class="fas fa-times delete-icon"> </i></button>
 
                             </td>
 
